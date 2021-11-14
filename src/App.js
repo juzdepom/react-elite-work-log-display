eff41800fb31c68608455ce86b06a8a81d023602
Julia Gao Miller <julia.gao.miller@gmail.com>
2021-11-14 19:41:12 +0700

    successfully displaying data from Elite Web co airtable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
+import { apiKey, baseId } from "./airtableIds";
+
+import React, { useEffect, useState } from "react";
+import Airtable from "airtable";
+
+const base = new Airtable({ apiKey: apiKey }).base(baseId);
+
 function App() {
+
+  const [entries, setEntries] = useState([]);
+
+  //this method pulls in the data
+  useEffect(() => {
+   //pull in the 'goals' table view data
+   base("entries")
+     .select({ view: "Grid view" })
+     .eachPage((records, fetchNextPage) => { 
+       setEntries(records);
+       // console.log(records)
+       fetchNextPage();
+     });
+  }, []);
+
+  let total = 'X hours X min'
+
   return (
-    <div>
+    <div style={{textAlign: "center", fontFamily: "'Roboto', sans-serif"}}>
+      <h1>ELITEWEB.Co Work Log</h1>
+      <p>Total time worked: {total}  
+      <br/>
+      <span><i>next task: figure out how to calculate total time worked</i></span>
+      </p>
+      
+      <h3>All Entries</h3>
+      {entries.map((entry, index) => (
+        <div key={index}>
+          <span>🗓{entry.fields.date} </span>
+          <span>🕐{entry.fields.minutes} min – </span>
+          <span><i>#{entry.fields.tag}</i></span>
+          <br/>
+          <span><i>{entry.fields.description}</i></span>
+          <br/>
+          <br/>
+        </div>
+      ))}
     </div>
   );
 }
